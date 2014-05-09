366040f351488a2e01128ac46ea53c665475905a
Arita Iori <hermitage1029@gmail.com>
2014-05-10 07:43:18 +0900

    テストプロジェクトのアップ

---
+++ Assets/Horming.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Horming : MonoBehaviour {
+
+	GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find("Main Camera");
+	}
+
+
+	// Update is called once per frame
+	void Update () {/*
+		transform.localPosition.x = target.transform.localPosition.x;
+		transform.localPosition.y = target.transform.localPosition.y;
+		transform.localPosition.z = target.transform.localPosition.z + 1;*/
+	}
+}
---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	public float x = 0.0f;
+	public float y = 0.0f;
+	public float z = 0.0f;
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.RightArrow)) 
+			y = 5.0f;
+
+		if (Input.GetKey(KeyCode.LeftArrow)) 
+			y = -5.0f;
+	
+		transform.Rotate(0, y, 0);
+
+		if (Input.GetKey(KeyCode.UpArrow)) 
+			x = -5.0f;
+			
+
+		if (Input.GetKey(KeyCode.DownArrow)) 
+			x = 5.0f;
+
+		transform.Rotate(x, 0, 0);
+
+		x = 0;
+		y = 0;
+		z = 0;
+
+		transform.Translate (0,0,0.07f);
+
+
+	}
+}
