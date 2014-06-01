f12fd86e1ae26e759eddffb826a2ea7eeba1cac6
kajikawa <kj1ktk@gmail.com>
2014-06-02 03:19:22 +0900

    修正

---
+++ Assets/Script/GameOverScreen.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScreen : MonoBehaviour {
+
+	public GUISkin skin;
+	int score;
+	int windows;
+	string state;
+
+	void ScoreKeeper(int endScore,int throughWindows) {
+		score = endScore;
+		windows = throughWindows;
+	}
+
+	void EndGame() {
+		enabled = true;
+		state = "End Game";
+		StartCoroutine("processing");
+		Rect rect = new Rect(Screen.width/4, Screen.height/2,Screen.width/2,Screen.height/4);
+		bool isClicked = GUI.Button(rect,"タイトル画面へ");
+		if (isClicked) {
+			Application.LoadLevel("Title");
+		}
+	}
+
+	IEnumerator processing() {
+		yield return new WaitForSeconds(3.0f);
+		state = "";
+		yield return new WaitForSeconds(0.5f);
+		state = "Show Score";
+	}
+
+	void OnGUI() {
+		GUI.skin = skin;
+		int sw = Screen.width;
+		int sh = Screen.height;
+
+		if (state == "End Game") {
+			Rect rect = new Rect(0, 0.2f * sh, sw, 0.3f * sh);
+			GUI.Label(rect, "GAME OVER!", "message");
+		}
+		if (state == "Show Score") {
+			Rect rect = new Rect(0, 0, sw, 0.3f * sh);
+			GUI.Label(rect, "Window:"+windows.ToString()+"\nSCORE: " + score.ToString(), "gameover");
+			rect = new Rect(0, sh / 2, sw, sh / 4);
+			GUI.Label(rect, "Click to Exit", "result");
+		}
+	}
+}
---
+++ Assets/Script/MovePlayer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	public float x = 0.0f;
+	public float y = 0.0f;
+	public float z = 0.0f;
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.RightArrow)) 
+			y = 2.0f;
+
+		if (Input.GetKey(KeyCode.LeftArrow)) 
+			y = -2.0f;
+
+		if (Input.GetKey(KeyCode.UpArrow)) 
+			x = -2.0f;
+			
+		if (Input.GetKey(KeyCode.DownArrow)) 
+			x = 2.0f;
+
+		if(GameObject.Find("PlayerModel")){
+			transform.Rotate(0, y, 0);
+			transform.Rotate(x, 0, 0);
+
+			transform.Translate (0,0,4f);
+			}
+		//RotateAngle ();
+
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+
+	void RotateAngle(){
+		Debug.Log (transform.rotation.z);
+	}
+
+	void StartGame() {
+		enabled = true;
+	}
+}
