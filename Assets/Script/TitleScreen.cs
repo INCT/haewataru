d424af40f824a2096fb8f9797032f10b1eae465d
kajikawa <kj1ktk@gmail.com>
2014-06-02 03:19:22 +0900

    修正

---
+++ Assets/Script/TitleScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			Application.LoadLevel("Main");
+		}
+	}
+
+	void OnGUI () {
+		Rect rect = new Rect(Screen.width / 4,Screen.height / 2, Screen.width/2, Screen.height / 4);
+		bool isClicked = GUI.Button(rect, "GAME START!");
+		if (isClicked) {
+			Application.LoadLevel("Main");
+		}
+	}
+}
