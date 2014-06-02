d859861d28fd4ab08389e4706c123a85a7268451
kajikawa <kj1ktk@gmail.com>
2014-06-03 03:10:48 +0900

    スコアの計算

--- Assets/Script/TimeKeeper.cs
+++ Assets/Script/TimeKeeper.cs
@@ -4,22 +4,22 @@ using System.Collections;
 public class TimeKeeper : MonoBehaviour {
 
 	public GUISkin skin;
-	double time;
+	float time;
 
 	void Update () {
 		time += Time.deltaTime;
 	}
 	void StartGame() {
 		enabled = true;
-		time = 0.0;
+		time = 0.0f;
 	}
 	void EndGame() {
 		enabled = false;
 	}
 	
 	void OnGUI() {
-	GUI.skin = skin;
-	Rect rect = new Rect(0, 0, Screen.width, Screen.height);
-	GUI.Label(rect, "Time: " + time.ToString(), "Time");
+		GUI.skin = skin;
+		Rect rect = new Rect(0, 0, Screen.width, Screen.height);
+		GUI.Label(rect, "Time: " + Mathf.Ceil(time).ToString(), "Time");
 	}
 }
