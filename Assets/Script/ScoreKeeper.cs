e43faa85b58d1bb79bab8d82b8e8708218aec29a
valbeat <kajikawa@valbeat.local>
2014-06-02 17:05:43 +0900

    ScoreKeeper

--- Assets/Script/ScoreKeeper.cs
+++ Assets/Script/ScoreKeeper.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class ScoreKeeper : MonoBehaviour {
-
+	public GUISkin skin;
+	int score;
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnGUI() {
+		GUI.skin = skin;
+		int sw = Screen.width;
+		int sh = Screen.height;
+
+		string scoreText = "SCORE: "+ score.ToString();
+		GUI.Label(new Rect(10,10, sw / 2, sh / 4),scoreText,"score");
 	}
 }
