9b91527ae932c35bd5bb3312b8a65b124aa61701
valbeat <kajikawa@valbeat.local>
2014-06-03 12:03:11 +0900

    窓通過すると+100ポイント

---
+++ Assets/Script/Goal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+	bool cleared;
+	// Use this for initialization
+	void Start () {
+		cleared = false;
+	}
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "PlayerModel") {
+			cleared = true;
+			GameObject.FindWithTag("Player").BroadcastMessage("Goal");
+		}
+	}
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.tag == "PlayerModel") {
+			//暗転して次のレベルへ
+		}
+	}
+}
