6e21c9432f4009273bb1f0631434245b9fe617fc
valbeat <kj1ktk@gmail.com>
2014-06-08 00:54:28 +0900

    add Scenes

--- Assets/Script/TitleScreen.cs
+++ Assets/Script/TitleScreen.cs
@@ -9,10 +9,24 @@ public class TitleScreen : MonoBehaviour {
 	}
 
 	void OnGUI () {
-		Rect rect = new Rect(Screen.width / 4,Screen.height / 2, Screen.width/2, Screen.height / 4);
-		bool isClicked = GUI.Button(rect, "GAME START!");
-		if (isClicked) {
+		int sw = Screen.width;
+		int sh = Screen.height;
+		Rect startButton = new Rect(Screen.width / 4,Screen.height / 2, Screen.width/2, Screen.height / 4);
+		Rect howToButton = new Rect(Screen.width / 4,Screen.height / 2, Screen.width/2, Screen.height / 4);
+		Rect scoreButton = new Rect(Screen.width / 4,Screen.height / 2, Screen.width/2, Screen.height / 4);
+		Rect optionButton = new Rect(Screen.width / 4,Screen.height / 2, Screen.width/2, Screen.height / 4);
+		if (GUI.Button(startButton, "はじめる")) {
 			Application.LoadLevel("Stage1");
 		}
+		if (GUI.Button(howToButton,"遊び方")) {
+			Application.LoadLevel("Tutorial");
+		}
+		if (GUI.Button(scoreButton,"記録")) {
+			Application.LoadLevel("Record");
+		}
+		if (GUI.Button(optionButton,"設定")) {
+			Application.LoadLevel("Option");
+		}
+
 	}
 }
