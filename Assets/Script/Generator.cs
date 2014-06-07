528745f2519ed3edce7019851c50f401436fdd05
valbeat <kj1ktk@gmail.com>
2014-06-07 23:07:01 +0900

    add Generator

---
+++ Assets/Script/Generator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+	public float intervalMin = 0.5f;
+	public float intervalMax = 1.5f;
+	public GameObject prefab;
+	
+	void Update () {
+		StartCoroutine("waitTime");
+		Vector3 position = new Vector3(Random.Range(-20,20), Random.Range(3,30), Random.Range(-20,20));
+		Instantiate(prefab, position, Random.rotation);
+	}
+	IEnumerator waitTime() {
+		yield return new WaitForSeconds(Random.Range(intervalMin, intervalMax));
+	}
+}
