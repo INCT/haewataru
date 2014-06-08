dfbe67d543b36647dc8d3ec2fd650ba01509856c
valbeat <kj1ktk@gmail.com>
2014-06-08 13:27:34 +0900

    レイアウト調整

--- Assets/Script/TitleScreen.cs
+++ Assets/Script/TitleScreen.cs
@@ -11,21 +11,21 @@ public class TitleScreen : MonoBehaviour {
 	void OnGUI () {
 		int sw = Screen.width;
 		int sh = Screen.height;
-		Rect startButton = new Rect(sw / 4,sh / 2, sw / 2, sh / 8);
-		Rect howToButton = new Rect(sw / 4.5f,sh / 1.5f, sw / 6, sh / 8);
-		Rect scoreButton = new Rect(sw / 3,sh / 1.5f, sw / 6, sh / 8);
-		Rect optionButton = new Rect(sw / 1.75f,sh / 1.5f, sw / 6, sh / 8);
-		if (GUI.Button(startButton, "はじめる")) {
+		GUILayout.BeginArea(new Rect(sw/4,sh/2,sw/2,sh/2));
+		if (GUILayout.Button("はじめる",GUILayout.MinHeight(sh/6))) {
 			Application.LoadLevel("Stage1");
 		}
-		if (GUI.Button(howToButton,"遊び方")) {
+		GUILayout.BeginHorizontal();
+		if (GUILayout.Button("遊び方",GUILayout.MinHeight(sh/6))) {
 			Application.LoadLevel("Tutorial");
 		}
-		if (GUI.Button(scoreButton,"記録")) {
+		if (GUILayout.Button("記録",GUILayout.MinHeight(sh/6))) {
 			Application.LoadLevel("Record");
 		}
-		if (GUI.Button(optionButton,"設定")) {
+		if (GUILayout.Button("設定",GUILayout.MinHeight(sh/6))) {
 			Application.LoadLevel("Option");
 		}
+		GUILayout.EndHorizontal();
+		GUILayout.EndArea();
 	}
 }
