111d72a5ade677cb7ca847015f95d99d4586743d
valbeat <kj1ktk@gmail.com>
2014-06-08 15:47:45 +0900

    add Plugins

--- Assets/Script/MovePlayer.cs
+++ Assets/Script/MovePlayer.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class MovePlayer : MonoBehaviour {
-
+	private static bool started = false;
 	public float x = 0.0f;
 	public float y = 0.0f;
 	public float z = 0.0f;
@@ -15,60 +15,61 @@ public class MovePlayer : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		if(started) {
+			if (Input.GetKey(KeyCode.RightArrow)) 
+				y = 2.0f;
 
-		if (Input.GetKey(KeyCode.RightArrow)) 
-			y = 2.0f;
+			if (Input.GetKey(KeyCode.LeftArrow)) 
+				y = -2.0f;
 
-		if (Input.GetKey(KeyCode.LeftArrow)) 
-			y = -2.0f;
+			if (Input.GetKey(KeyCode.UpArrow)) 
+				x = -2.0f;
 
-		if (Input.GetKey(KeyCode.UpArrow)) 
-			x = -2.0f;
+			if (Input.GetKey(KeyCode.DownArrow)) 
+				x = 2.0f;
 
-		if (Input.GetKey(KeyCode.DownArrow)) 
-			x = 2.0f;
+			touchInfo = "Screen:"+Screen.width+"*"+Screen.height+"\n";
 
-		touchInfo = "Screen:"+Screen.width+"*"+Screen.height+"\n";
+			for (int i=0;i<Input.touchCount; i++) {
+				touchInfo += i + " : x=" + Input.GetTouch (i).position.x + " y=" + Input.GetTouch (i).position.y + "\n";
+			} 
 
-		for (int i=0;i<Input.touchCount; i++) {
-			touchInfo += i + " : x=" + Input.GetTouch (i).position.x + " y=" + Input.GetTouch (i).position.y + "\n";
-		} 
+			if (Input.touchCount >= 2) {
+				int w = Screen.width;
+				int h = Screen.height;
 
-		if (Input.touchCount >= 2) {
-			int w = Screen.width;
-			int h = Screen.height;
+				float x0 = Input.GetTouch(0).position.x-w/2;
+				float x1 = Input.GetTouch(1).position.x-w/2;
+				float y0 = Input.GetTouch(0).position.y-h/2;
+				float y1 = Input.GetTouch(1).position.y-h/2;
 
-			float x0 = Input.GetTouch(0).position.x-w/2;
-			float x1 = Input.GetTouch(1).position.x-w/2;
-			float y0 = Input.GetTouch(0).position.y-h/2;
-			float y1 = Input.GetTouch(1).position.y-h/2;
-
-			if(x0 > 0 != x1 > 0){
-				if(y0 > 0 == y1 > 0){
-					x = -2.0f * (y0+y1)/h;
+				if(x0 > 0 != x1 > 0){
+					if(y0 > 0 == y1 > 0){
+						x = -2.0f * (y0+y1)/h;
+					}
+					if(x0<0){
+						y = -2.0f * (y1-y0)/h;
+					}else{
+						y = -2.0f * (y0-y1)/h;
+					}
 				}
-				if(x0<0){
-					y = -2.0f * (y1-y0)/h;
-				}else{
-					y = -2.0f * (y0-y1)/h;
-				}
+
+				touchInfo += x0+","+y0+" & "+x1+","+y1+"\n";
+
 			}
 
-			touchInfo += x0+","+y0+" & "+x1+","+y1+"\n";
+			if(GameObject.Find("PlayerModel")){
+				transform.Rotate(0, y, 0);
+				transform.Rotate(x, 0, 0);
 
+				transform.Translate (0,0,2.0f);
+			}
+			//RotateAngle ();
+
+			x = 0;
+			y = 0;
+			z = 0;
 		}
-
-		if(GameObject.Find("PlayerModel")){
-			transform.Rotate(0, y, 0);
-			transform.Rotate(x, 0, 0);
-
-			transform.Translate (0,0,2.0f);
-		}
-		//RotateAngle ();
-
-		x = 0;
-		y = 0;
-		z = 0;
 	}
 
 	void RotateAngle(){
@@ -76,7 +77,9 @@ public class MovePlayer : MonoBehaviour {
 	}
 
 	void StartGame() {
-		enabled = true;
+		if (!started) {
+			started = true;
+		}
 	}
 	void EndGame() {
 		enabled = false;
