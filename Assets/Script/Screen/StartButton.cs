b0896729133b706d72072ab35a5d11701ff5053c
valbeat <kj1ktk@gmail.com>
2014-06-17 00:08:08 +0900

    スタートボタン追加

---
+++ Assets/Script/Screen/StartButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+	public GameObject GameObjectPrefab;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown () {
+		Application.LoadLevel("Stage1");
+		Instantiate(GameObjectPrefab,new Vector3(0,0,0), Quaternion.identity);
+	}
+}
