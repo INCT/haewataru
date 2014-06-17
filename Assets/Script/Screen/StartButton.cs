75ff1eda085366c149a0432d4bbd36315d7fd546
valbeat <kj1ktk@gmail.com>
2014-06-17 18:22:24 +0900

    delete comment

--- Assets/Script/Screen/StartButton.cs
+++ Assets/Script/Screen/StartButton.cs
@@ -3,15 +3,6 @@ using System.Collections;
 
 public class StartButton : MonoBehaviour {
 	public GameObject GameObjectPrefab;
-	// Use this for initialization
-	void Start () {
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-	}
 
 	void OnMouseDown () {
 		Application.LoadLevel("Stage1");
