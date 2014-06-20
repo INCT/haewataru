2912bf70352a2ce92f0bbfe341c32a7694fcb27e
valbeat <kj1ktk@gmail.com>
2014-06-20 11:19:37 +0900

    アイテム実装

---
+++ Assets/Script/GameController/Item.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+	public GameObject itemFx;
+	public AudioClip itemSE;
+	public int amount;
+	public string itemType = 'Apple';
+	bool cleard;
+	void Start () {
+		cleard = false;
+	}
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "PlayerModel" && cleared == false) {
+
+			switch (itemType) {
+				case Apple:
+				cleared = true;
+				GameObject.FindWithTag("Player").BroadcastMessage("LifePlus",amount);
+				break;
+			}
+
+		}
+	}
+
