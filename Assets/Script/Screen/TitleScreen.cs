f0bdbe2285c774e2a0b84951e099e86e7d2576fa
Arita Iori <hermitage1029@gmail.com>
2014-06-20 11:48:02 +0900

    画像とか細かい修正

--- Assets/Script/Screen/TitleScreen.cs
+++ Assets/Script/Screen/TitleScreen.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class TitleScreen : MonoBehaviour {
 
+	public Texture2D score_im; 
+
 	public GameObject GameObjectPrefab;
 	void Update () {
 		if (Input.GetButtonDown("Jump")) {
 			Application.LoadLevel("Main");
 		}
+		/*
+		if (GameObject.FindWithTag("Recode Button")) {
+			Application.LoadLevel("Recode");
+		}*/
+
 	}
 
 	void OnGUI () {
@@ -18,7 +25,7 @@ public class TitleScreen : MonoBehaviour {
 		if (GUILayout.Button("遊び方",GUILayout.MinHeight(sh/6))) {
 			Application.LoadLevel("Tutorial");
 		}
-		if (GUILayout.Button("記録",GUILayout.MinHeight(sh/6))) {
+		if (GUI.Button( new Rect(sh/6,sh/6,sh/4,sh/6),score_im)) {
 			Application.LoadLevel("Record");
 		}
 		if (GUILayout.Button("設定",GUILayout.MinHeight(sh/6))) {
