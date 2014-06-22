fa217faeda369ac5c0df0426847765211f19be71
valbeat <kj1ktk@gmail.com>
2014-06-22 16:54:31 +0900

    衝突判定の修正

--- Assets/Script/ObjectController/Item.cs
+++ Assets/Script/ObjectController/Item.cs
@@ -5,16 +5,12 @@ public class Item : MonoBehaviour {
 	public GameObject itemFx;
 	public AudioClip itemSE;
 	public int amount = 100;
-	public char itemType = 'A';
-	bool cleared = false;
 	void Start () {
-		cleared = false;
 	}
 	void OnTriggerEnter(Collider other) {
-		if(other.gameObject.tag == "PlayerModel" && cleared == false) {
-				cleared = true;
-				//GameObject.FindWithTag("Player").BroadcastMessage("PlusScore",amount);
-				Object.Destroy(this);
+		if(other.gameObject.tag == "PlayerModel") {
+				GameObject.FindWithTag("Player").BroadcastMessage("PlusScore",amount);
+				Object.Destroy(gameObject);
 		}
 	}
 }
