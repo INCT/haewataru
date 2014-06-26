165aff3c2b31ea50f32c82f3a3d5a777b3bc3a73
Arita Iori <hermitage1029@gmail.com>
2014-06-27 00:35:26 +0900

    DurovisDiveの画像の追加

--- Assets/Script/Screen/Option.cs
+++ Assets/Script/Screen/Option.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Option : MonoBehaviour {
 	public GUISkin skin;
+	public Texture2D dive_on;
+	public Texture2D dive_off;
+	Texture2D dive_im;
 	public Texture2D back_im;
 
 	string isDiving = "ON";
@@ -14,27 +17,38 @@ public class Option : MonoBehaviour {
 
 		isDiving = PlayerPrefs.GetString ("isDiving");
 
-		if(!PlayerPrefs.HasKey("isDiving"))
-			isDiving = "OFF";
+		if (!PlayerPrefs.HasKey ("isDiving")) {
+						isDiving = "OFF";
+						dive_im = dive_off;
+		}
 
-		GUILayout.BeginArea(new Rect(sw/4,sh/2,sw/2,sh/3));
-		if (GUILayout.Button("Durovis Dive : " + isDiving,GUILayout.MinHeight(sh/6))) {
+		if (isDiving == "ON")
+						dive_im = dive_on;
+		else 
+			dive_im = dive_off;
+
+
+		GUILayout.BeginArea(new Rect(sw/4,sh/5*2,sw/2,sh/3));
+		//if (GUILayout.Button("Durovis Dive : " + isDiving,GUILayout.MinHeight(sh/6))) {
+		if (GUI.Button(new Rect(0,0,sw/2,sh/3), dive_im)) {
 			if(isDiving == "OFF"){
 				isDiving = "ON";
 				PlayerPrefs.SetString("isDiving",isDiving);
-				Debug.Log("ON");
+				dive_im = dive_on;
+				//Debug.Log("ON");
 			}
 			else if(isDiving == "ON"){
 				isDiving = "OFF";
 				PlayerPrefs.SetString("isDiving",isDiving);
-				Debug.Log("OFF");
+				dive_im = dive_off;
+				//Debug.Log("OFF");
 			}
 		}
 		GUILayout.EndArea();
 
-		GUILayout.BeginArea(new Rect(sw/4,sh/4*3,sw/2,sh/3));
+		GUILayout.BeginArea(new Rect(sw/4,sh/5*4,sw/2,sh/4));
 
-		if (GUILayout.Button(back_im,GUILayout.MinHeight(sh/6))) {
+		if (GUILayout.Button(back_im,GUILayout.MinHeight(sh/10))) {
 			Application.LoadLevel("Title");
 		}
 
