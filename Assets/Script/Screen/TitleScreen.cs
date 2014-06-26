b9a7a21ba67e95e95ef22e2643eb157564c5c859
Arita Iori <hermitage1029@gmail.com>
2014-06-26 20:24:07 +0900

    スタートボタン修正

--- Assets/Script/Screen/TitleScreen.cs
+++ Assets/Script/Screen/TitleScreen.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class TitleScreen : MonoBehaviour {
 
+	public Texture2D start_im;
 	public Texture2D score_im;
 	public Texture2D option_im;
-	public Texture2D tutorial_im; 
+	public Texture2D tutorial_im;
+	//public GUIStyle buttonStyle;
 
 	public GameObject GameObjectPrefab;
 	void Update () {
@@ -22,15 +24,20 @@ public class TitleScreen : MonoBehaviour {
 	void OnGUI () {
 		int sw = Screen.width;
 		int sh = Screen.height;
-		GUILayout.BeginArea(new Rect(0,sh/2 + 150,sw,sh/2));
+		GUILayout.BeginArea(new Rect(0,sh/2,sw,sh));
 		GUILayout.BeginHorizontal();
-		if (GUI.Button( new Rect(sw/8*1,0,sw/4,sh/6), tutorial_im)) {
+
+		if (GUI.Button( new Rect(sw/4,0,sw/2,sh/4), start_im)) {
+			Application.LoadLevel("Stage1");
+			Instantiate(GameObjectPrefab,new Vector3(0,0,0), Quaternion.identity);
+		}
+		if (GUI.Button( new Rect(sw/8*1,sh/4,sw/4,sh/6), tutorial_im)) {
 			Application.LoadLevel("Tutorial");
 		}
-		if (GUI.Button( new Rect(sw/8*3,0,sw/4,sh/6), score_im)) {
+		if (GUI.Button( new Rect(sw/8*3,sh/4,sw/4,sh/6), score_im)) {
 			Application.LoadLevel("Record");
 		}
-		if (GUI.Button( new Rect(sw/8*5,0,sw/4,sh/6), option_im)) {
+		if (GUI.Button( new Rect(sw/8*5,sh/4,sw/4,sh/6), option_im)) {
 			Application.LoadLevel("Option");
 		}
 		GUILayout.EndHorizontal();
